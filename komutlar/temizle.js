0d73bd7e980faf1999b2ae9f48017575bf66ab95
OmerEksi <42853845+OmerEksi@users.noreply.github.com>
2018-08-31 04:39:09 +0300

    Add files via upload

---
+++ komutlar/temizle.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+
+exports.run = function(client, message, args) {
+	const sayi = args.slice(0).join(' ');
+
+
+  if (sayi.length < 1 ) {
+	return message.reply("Silmem İçin Bir Miktar Belirt")
+  } else {   
+            message.channel.bulkDelete(sayi + 1);
+      message.channel.send("**" + sayi + "**adet mesaj sildim").then(msg => {
+	      msg.delete(5000)
+      });
+	  }
+
+
+};
+
+exports.conf = {
+  enabled: true, 
+  guildOnly: false, 
+  aliases: [],
+  permLevel: 0 
+};
+
+exports.help = {
+  name: 'temizle', 
+  description: 'Belirtilen miktarda mesaj siler',
+  usage: 'temizle <miktar>'
+};
